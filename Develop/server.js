f099fe33f6abf92bcb087a7de03713693a08d9df
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-03-14 15:43:26 -0600

    created server.js file

---
+++ Develop/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// will need to require in routes below !!!! ---------------------------------------------------------------------
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
